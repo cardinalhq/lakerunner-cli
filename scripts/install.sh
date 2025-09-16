0550949c13248aa148993125e44ab19d7441b0d8
Kunal Kundaje <kunal@cardinalhq.io>
2025-09-16 12:44:20 -0700

    install.sh: Update Grafana plugin version

--- scripts/install.sh
+++ scripts/install.sh
@@ -745,7 +745,7 @@ grafana:
     type: ClusterIP
     port: 3000
   plugins:
-    - "https://github.com/cardinalhq/cardinalhq-lakerunner-datasource/releases/download/v1.2.0-rc.10/cardinalhq-lakerunner-datasource.zip;cardinalhq-lakerunner-datasource"
+    - "https://github.com/cardinalhq/cardinalhq-lakerunner-datasource/releases/download/v1.2.0-rc.12/cardinalhq-lakerunner-datasource.zip;cardinalhq-lakerunner-datasource"
   datasources:
     datasources.yaml:
       apiVersion: 1
